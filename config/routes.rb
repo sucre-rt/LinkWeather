ba3f51a84b820c1c365e155bdaa8f9237fdc789b
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-12-12 22:34:19 +0900

    login function create show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
 
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations'
+  }
+
+  devise_scope :user do
+    get "users/:id", :to => "users/confirmations#show"
+  end
+
   resources :weathers, only: [:index] do
     collection do
       post :index
     end
   end
+  
 end
