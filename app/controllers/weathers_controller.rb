a60c3dc096ac55100704a7302a9fff19c570e549
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-12-12 21:33:07 +0900

    error handling

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -11,16 +11,20 @@ class WeathersController < ApplicationController
     uri = URI.parse("https://map.yahooapis.jp/geocode/V1/geoCoder?appid=#{client_id}&query=#{URI.encode(city)}&output=json")
     json = Net::HTTP.get(uri)
     area_result = JSON.parse!(json)
-    lon, lat = area_result['Feature'][0]['Geometry']['Coordinates'].split(',')
-    # openweather
-    api_key = Rails.application.credentials.open_wether[:api_key]
-    uri = URI.parse("http://api.openweathermap.org/data/2.5/forecast?lat=#{lat}&lon=#{lon}&units=metric&APPID=#{api_key}&lang=ja&units=metric")
-    json = Net::HTTP.get(uri)
-    @result = JSON.parse(json)
-    @result_li = @result["list"]
-    @area = search_params[:city]
-    @today = Date.today
-    @now = DateTime.now
+    unless area_result["ResultInfo"]["Count"] == 0
+      lon, lat = area_result['Feature'][0]['Geometry']['Coordinates'].split(',')
+      # openweather
+      api_key = Rails.application.credentials.open_wether[:api_key]
+      uri = URI.parse("http://api.openweathermap.org/data/2.5/forecast?lat=#{lat}&lon=#{lon}&units=metric&APPID=#{api_key}&lang=ja&units=metric")
+      json = Net::HTTP.get(uri)
+      @result = JSON.parse(json)
+      @result_li = @result["list"]
+      @area = search_params[:city]
+      @today = Date.today
+      @now = DateTime.now
+    else
+      @result = ""
+    end
   end
 
   private
