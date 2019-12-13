fe69823db103a6c52061832b31938511b3a055a7
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-12-13 15:53:26 +0900

    acount edit function

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:profile_update]
 
   # GET /resource/sign_up
   # def new
@@ -38,7 +38,22 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  def profile_edit
+  end
+
+  def profile_update
+    if current_user.update(account_update_params)
+	    redirect_to profile_edit_path, notice: 'プロフィールを更新しました'
+    else
+      render "profile_edit"
+    end
+  end
+
+  protected
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :my_area])
+   end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get "users/:id", :to => "users/confirmations#show"
+    get "profile_edit", :to => "users/registrations#profile_edit", as: 'profile_edit'
+    patch 'profile_update', to: 'users/registrations#profile_update', as: 'profile_update'
   end
 
   resources :weathers, only: [:index] do
