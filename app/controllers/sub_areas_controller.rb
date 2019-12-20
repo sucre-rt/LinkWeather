47b45a4599fea1ed18e204dc3fb464412d5a207d
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-12-20 19:35:25 +0900

    change asynchronous

--- app/controllers/sub_areas_controller.rb
+++ app/controllers/sub_areas_controller.rb
@@ -1,26 +1,25 @@
 class SubAreasController < ApplicationController
+  before_action :set_variables
 
   def create
     if current_user != nil
       sub_area = current_user.sub_areas.build(area: params[:area])
-      if sub_area.save
-        redirect_to mypage_path(current_user.id)
-      else
-        redirect_back(fallback_location: root_path)
-      end
+      sub_area.save
     end
   end
 
   def delete
-    @area = params[:area]
     if @area != nil && current_user != nil
       @sub_area = current_user.sub_areas.find_by(area: @area)
-      if @sub_area.destroy
-        redirect_to mypage_path(current_user.id)
-      else
-        redirect_back(fallback_location: root_path)
-      end
+      @sub_area.destroy
     end
+  end
+
+  private
+
+  def set_variables
+    @area = params[:area]
+    @area_id = "#sub-area-link-#{ @area }"
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :tweets
   has_many :sub_areas
+
+  def already_area?(area)
+    self.sub_areas.exists?(area: area)
+  end
+
 end
