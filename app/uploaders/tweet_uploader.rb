9f181030959355d9c269bbfde6a536af503658c4
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-12-24 17:38:10 +0900

    s3

--- app/uploaders/tweet_uploader.rb
+++ app/uploaders/tweet_uploader.rb
@@ -5,8 +5,8 @@ class TweetUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [600, 600]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
