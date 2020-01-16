0c069d866b2fda9385e7a4aa0426ad4e36db0b7a
sucre-rt <sucre_fraises@ezweb.ne.jp>
2020-01-16 20:04:38 +0900

    add sub_area validation

--- spec/models/sub_areas_spec.rb
+++ spec/models/sub_areas_spec.rb
@@ -18,5 +18,13 @@ RSpec.describe SubArea, type: :model do
       sub_area.valid?
       expect(sub_area.errors[:user]).to include("を入力してください")
     end
+    
+    it '同じ組み合わせのレコードは重複登録できない' do
+      sub_area = create(:sub_area)
+      sub_area_2 = build(:sub_area)
+      sub_area_2.valid?
+      expect(sub_area_2.errors[:area]).to include("はすでに存在します")
+    end
   end
+  
 end
